cb78baf44d949e61742f714bf757d919eb15d414
luofu <mqa010225@163.com>
2024-04-03 17:51:50 +0800

    init repo

---
+++ setup.py
@@ -0,0 +1,16 @@
+# !/usr/bin/env python
+# -*- coding : utf-8 -*-
+# @author : AllenDEricDAlexander
+# blog : https://allendericdalexander.github.io/
+
+from distutils.core import setup
+
+setup(
+    name='c2r',
+    version='0.1.1',
+    description='This repository is translated from C language code to Rust code. Please refer to the GPL-3 open source protocol and BY-NC-SA Creative Commons license',
+    author='AllenDEricDAlexander',
+    author_email='mqa010225@gmail.com',
+    url='https://github.com/AllenDEricDAlexander/c2r',
+    packages=find_packages()
+)
